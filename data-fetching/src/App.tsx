00559ef010f774a86ae465367dc609df19d87b3e
Otávio Augusto Gonçalves Nogueira <otavio.a.nogueira@hotmail.com>
2022-03-19 14:48:43 -0300

    Maneira comum de se fazer chamada a API

--- data-fetching/src/App.tsx
+++ data-fetching/src/App.tsx
@@ -1,6 +1,19 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+
+type Repositorios = {
+  nickname:string;
+  description:string;
+}
 
 function App() {
+  const [repositorios,setRepositorios] = useState<Repositorios[]>([])
+  useEffect(() =>{
+    fetch('https://api.github.com/users/otavioaugusto1/repos')
+    .then(response => response.json())
+    .then(data => {
+      setRepositorios(data)
+    })
+  })
 
   return (
     <div className="App">
